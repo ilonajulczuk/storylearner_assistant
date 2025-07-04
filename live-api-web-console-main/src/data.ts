7e34788d3e1aaac2d530893282678394254e0759
smogork <krzysztof.smogor@wp.pl>
2025-07-04 16:17:58 +0200

    Add missing sentence

--- live-api-web-console-main/src/data.ts
+++ live-api-web-console-main/src/data.ts
@@ -12,4 +12,5 @@ export let spanish_sentences: string[] = [
     "Por la mañana, una manzana desapareció.",
     "Entonces el segundo hijo vigiló.",
     "Él durmió también.",
+    "Por la mañana, una manzana desapareció."
 ];
