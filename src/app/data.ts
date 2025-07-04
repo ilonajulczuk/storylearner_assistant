86273db60ce6b5ca00e7fb36f2d4af3038e5b3f2
smogork <krzysztof.smogor@wp.pl>
2025-07-04 15:52:37 +0200

    Hardcode one spanish page

---
+++ src/app/data.ts
@@ -0,0 +1,15 @@
+export let spanish_sentences: string[] = [
+    "Un rey tenía un jardín bonito.",
+    "En el jardín había un árbol.",
+    "El árbol tenía manzanas de oro.",
+    "Las manzanas fueron contadas.",
+    "Cada noche, una manzana desaparecía.",
+    "El rey estaba enfadado.",
+    "Le dijo al jardinero que vigilara el árbol por la noche.",
+    "El jardinero envió a su primer hijo.",
+    "El hijo vigiló.",
+    "Por la noche, él durmió.",
+    "Por la mañana, una manzana desapareció.",
+    "Entonces el segundo hijo vigiló.",
+    "Él durmió también.",
+]
